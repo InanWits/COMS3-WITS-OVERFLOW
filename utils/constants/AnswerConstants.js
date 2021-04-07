4a2ba0b9f68b8ab08495cb8bd443906b3bcc7ada
Koketso31 <1908676@students.wits.ac.za>
2021-04-07 16:12:41 +0200

    added database tables constants

---
+++ utils/constants/AnswerConstants.js
@@ -0,0 +1,8 @@
+module.exports = {
+    table_name : "Answer",
+    id : "id",
+    question_id : "question_id",
+    student_id : "student_id",
+    answer : "answer",
+    answer_date_time : "answer_date_time"
+}
