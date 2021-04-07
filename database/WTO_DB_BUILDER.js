ea215c5993eb39feda319253de4c892ed44cf2fd
SurpriceMasilela <1921674@students.wits.ac.za>
2021-04-07 18:33:34 +0200

    created query to build student table

--- database/WTO_DB_BUILDER.js
+++ database/WTO_DB_BUILDER.js
@@ -1,5 +1,16 @@
 const database = require('../utils/services/database');
 
+const studentConstants = require('../utils/constants/StudentConstants');
+
+const CREATE_STUDENT_TABLE = `create table ${studentConstants.table_name}(
+    ${studentConstants.id} int,
+    ${studentConstants.user_name} varchar(20) unique not null,
+    ${studentConstants.first_name} varchar(20) not null,
+    ${studentConstants.last_name} varchar(20) not null,
+    ${studentConstants.date_of_birth} date not null,
+    ${studentConstants.password} varchar(200) not null,
+    primary key (${studentConstants.id}))`;
+
 module.exports = {
 
     createDatabase: () => {
