ff78c9920b2fa8767979426d34dd854c36e6a227
Thabelang02 <1918469@students.wits.ac.za>
2021-04-07 16:59:34 +0200

    removed Dummy objects

--- tests/helpers/DummyHelper.test.js
+++
@@ -1,6 +0,0 @@
-const dummyHelper = require('../../helpers/DummyHelper');
-const {test} = require("@jest/globals");
-
-test('runs a dummy test', () => {
-   expect(dummyHelper.add(10, 10)).toBe(20);
-});
