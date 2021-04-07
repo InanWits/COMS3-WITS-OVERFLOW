f712905e3903926e360101c7aa3e83f64e48b3e3
MavhangeleMphoMavhangele <1900847@students.wits.ac.za>
2021-04-07 19:31:06 +0200

    added email attribute in the create query

--- database/WTO_DB_BUILDER.js
+++ database/WTO_DB_BUILDER.js
@@ -7,6 +7,7 @@ const CREATE_STUDENT_TABLE = `create table ${studentConstants.table_name}(
     ${studentConstants.user_name} varchar(20) unique not null,
     ${studentConstants.first_name} varchar(20) not null,
     ${studentConstants.last_name} varchar(20) not null,
+    ${studentConstants.email} varchar(30) not null,
     ${studentConstants.date_of_birth} date not null,
     ${studentConstants.password} varchar(200) not null,
     primary key (${studentConstants.id}))`;
