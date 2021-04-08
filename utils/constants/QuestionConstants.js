a4b730558518b42142e1331caf363ade5b90562d
MavhengeleMphoMavhengele <1900847@students.wits.ac.za>
2021-04-08 12:31:56 +0200

    Updated the question constants

--- utils/constants/QuestionConstants.js
+++ utils/constants/QuestionConstants.js
@@ -1,6 +1,6 @@
 module.exports = {
     table_name: "Question",
-    id: "id",
+    question_id: "question_id",
     student_id: "student_id",
     question: "question",
     post_date_time: "post_date_time"
