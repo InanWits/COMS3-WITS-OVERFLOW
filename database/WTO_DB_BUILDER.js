cb66d170900e2b0fc066b569e07dc119b876b721
SurpriceMasilela <1921674@students.wits.ac.za>
2021-04-08 12:26:16 +0200

    Changed the reference name from id to student id (line 10)

--- database/WTO_DB_BUILDER.js
+++ database/WTO_DB_BUILDER.js
@@ -1,9 +1,13 @@
 const database = require('../utils/services/database');
 
 const studentConstants = require('../utils/constants/StudentConstants');
+const questionConstants = require('../utils/constants/QuestionConstants');
+const answerConstants = require('../utils/constants/AnswerConstants');
+
+
 
 const CREATE_STUDENT_TABLE = `create table ${studentConstants.table_name}(
-    ${studentConstants.id} int,
+    ${studentConstants.student_id} int ,
     ${studentConstants.user_name} varchar(20) unique not null,
     ${studentConstants.first_name} varchar(20) not null,
     ${studentConstants.last_name} varchar(20) not null,
