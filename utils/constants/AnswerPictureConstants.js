1146502bdc04de986c68011b27a34425d972fa45
MavhengeleMphoMavhengele <1900847@students.wits.ac.za>
2021-04-24 01:07:54 +0200

    Removed models, controllers, constants for question and answer picture

--- utils/constants/AnswerPictureConstants.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    table_name : "AnswerPicture",
-    answer_picture_id : "answer_picture_id",
-    answer_id : "answer_id",
-    answer_picture_url : "answer_picture_url"
-}
--- utils/constants/QuestionPictureConstants.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    table_name : "QuestionPicture",
-    question_picture_id : "question_picture_id",
-    question_id : "question_id",
-    question_picture_url : "question_picture_url"
-}
