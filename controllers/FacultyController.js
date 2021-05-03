2ce8646fbfdc1ccf6b57831b5e26ad76403cfcc8
Masego-tech <1859136@students.wits.ac.za>
2021-05-03 21:04:27 +0200

    fixed school read by facultyId

--- controllers/FacultyController.js
+++ controllers/FacultyController.js
@@ -1,5 +1,7 @@
 const facultyModel = require('../models/FacultyModel');
+const schoolModel = require('../models/SchoolModel');
 
+const schoolConstants = require('../utils/constants/SchoolConstants');
 const responseHandler = require('../utils/services/ResponseHandler');
 
 const express = require('express');
@@ -16,6 +18,14 @@ router.post('/', (req, res) => {
 
 router.get('/', (req, res) => {
     facultyModel.readAllFaculties().then(
+        (response) => responseHandler.sendResponseOkay(response, res),
+        (err) => responseHandler.sendNotAcceptableResponse(err, res)
+    );
+});
+
+router.get(`/:${schoolConstants.faculty_id}/schools`, (req, res) => {
+    const facultyId = req.params[schoolConstants.faculty_id];
+    schoolModel.readSchools(facultyId).then(
         (response) => responseHandler.sendResponseOkay(response, res),
         (err) => responseHandler.sendNotAcceptableResponse(err, res)
     );
