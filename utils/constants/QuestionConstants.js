2e44eb8d8be98047680c07614ecc49f3e7833bfa
Thabelang02 <1918469@students.wits.ac.za>
2021-05-03 19:42:48 +0200

    removed some constant var

--- utils/constants/QuestionConstants.js
+++ utils/constants/QuestionConstants.js
@@ -2,8 +2,6 @@ module.exports = {
     table_name: "Question",
     question_id: "question_id",
     student_id: "student_id",
-    school_id: "school_id",
-    faculty_id : "faculty_id",
     topic_id: "topic_id",
     question: "question",
     question_picture_url : "question_picture_url",
