4b83fc16d5231050d364ef35fc0041d49db19b12
Koketso31 <1908676@students.wits.ac.za>
2021-05-06 16:09:59 +0200

    added a new request

--- public/js/models/QuestionModel.js
+++ public/js/models/QuestionModel.js
@@ -2,5 +2,9 @@ const questionEndPoint = 'Questions';
 
 const createQuestion = (QuestionData) => {
     return sendPostRequest(questionEndPoint, QuestionData);
+}
 
+const readQuestionAnswers = (questionId) => {
+    const endPoint = `${questionEndPoint}/${questionId}/answers`;
+    return sendGetRequest(endPoint);
 }
