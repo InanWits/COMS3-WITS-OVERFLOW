70f24583d5bf74fa4df706b7310ecc8cf8b7ba80
Thabelang02 <1918469@students.wits.ac.za>
2021-05-06 11:12:05 +0200

    modified to insert function to match database

--- models/QuestionModel.js
+++ models/QuestionModel.js
@@ -10,9 +10,7 @@ module.exports = {
     /*
        * QuestionJsonObject is a json object in this format
        * {
-       * student_id : "123456",
-       * school_id : "school"
-       * faculty_id : "1"
+       * student_id : "123456"
        * topic_id : "topic"
        * question : "question?"
        * question_picture_url : "picture"
@@ -22,8 +20,7 @@ module.exports = {
 
         return new Promise((resolve, reject) => {
 
-            const columnsToBeInserted = [ questionConstants.student_id, questionConstants.school_id, questionConstants.faculty_id, questionConstants.topic_id
-                , questionConstants.question,questionConstants.question_picture_url];
+            const columnsToBeInserted = [ questionConstants.student_id, questionConstants.topic_id, questionConstants.question,questionConstants.question_picture_url];
 
             const insertQuestion = queryHelper.buildInsertQuery(questionConstants.table_name, columnsToBeInserted, QuestionJsonObject);
 
