a522ce06e4022345c883e9e66a684541e11a1737
Koketso31 <1908676@students.wits.ac.za>
2021-05-06 16:08:44 +0200

    finished off the code for home page behaviour

--- public/js/controllers/HomePageController.js
+++ public/js/controllers/HomePageController.js
@@ -1,19 +1,81 @@
 $(window).on('load', () => {
 
-    const facultyContainer = $('#faculty-container');
+    let currentlySelected = null;
+    let isNavOpen = false;
 
-    readAllFaculties().then((faculties) => {
+    const iFrame = $('#content-holder');
+    const asideItems = $('.aside-item');
+    const header = $('header');
+    const section = $('section');
 
-        faculties.forEach((faculty) => {
-            const facultyItem = $('<div class="card-view">');
-            const facultyName = $('<p>');
+    const openNav = () => {
+        isNavOpen = true;
+        nav.width(250);
+        header.css('margin-left', "250px");
+        section.css('margin-left', "250px");
+    };
 
-            facultyName.text(faculty.faculty_name);
-            facultyItem.append(facultyName);
+    const closeNav = () => {
+        isNavOpen = false;
+        nav.width(0);
+        header.css('margin-left', "0");
+        section.css('margin-left', "0");
+    };
 
-            facultyContainer.append(facultyItem);
-        });
+    //gets the base URL
+    const getAppBaseUrl = () => {
+        let baseUrl = window.location.origin; //gets base url e.g.http://localhost:63343
+        let pathName = window.location.pathname; //gets the path from root to current folder e.g. /COMS3-WITS-OVERFLOW/public/html/HomePage.html
 
+        /*the main thing we care about from the path name
+        * is the path from the root to the html folder*/
+        pathName = pathName.split('html');
+        pathName = pathName[0]; ///COMS3-WITS-OVERFLOW/public/
+
+        baseUrl = `${baseUrl}${pathName}`;
+        return baseUrl;
+    };
+
+    asideItems.on('click', (e) => {
+        if (currentlySelected != null){
+            currentlySelected.classList = ['aside-item'];
+        }
+        e.currentTarget.classList = ['aside-item-selected'];
+        currentlySelected = e.currentTarget;
+
+        //get the attached URL
+        const attachedUrl = e.currentTarget.getAttribute('data-target-url');
+        const targetUrl = `${getAppBaseUrl()}${attachedUrl}`;
+
+        //console.log(targetUrl);
+        //open the target page in the iFrame
+        iFrame.attr('src', targetUrl);
     });
+
+    //select the first item by default
+    asideItems[0].click();
+
+    //create a reference to navigation and close button
+    const nav = $('#nav');
+    const closeNavBtn = $('#aside-back');
+
+    closeNavBtn.on('click', () => {
+        closeNav();
+    });
+
+    //create a reference to navigation button
+    const navButton = $('#nav-button');
+    navButton.on('click', () => {
+        if (isNavOpen){
+            closeNav();
+        }
+        else{
+            openNav();
+        }
+    });
+
+    //have the navigation open by default
+    openNav();
+
 
 });
