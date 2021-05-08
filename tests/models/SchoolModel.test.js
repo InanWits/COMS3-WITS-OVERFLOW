482f7aa0333ab22260061d58c3f94a0157f3a4f9
SurpriceMasilela <1921674@students.wits.ac.za>
2021-05-08 14:31:45 +0200

    Created tests for the school model

---
+++ tests/models/SchoolModel.test.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv');
+const {afterAll, beforeAll, test} = require("@jest/globals");
+
+const database = require("../../utils/services/database");
+
+const schoolModel = require("../../models/SchoolModel");
+
+beforeAll(async (done) => {
+    dotenv.config();
+    database.connectToDatabase().then(() => { done(); },
+        () => { done(); });
+});
+
+afterAll(async () => {
+    await database.getConnection().end();
+});
+
+test("test_read_school", () => {
+    schoolModel.readSchools().then((result) => {
+        expect(result).not.toBe(null);
+    })
+});
+
+
+/*
+
+afterAll(async () => {
+    await database.getConnection().end();
+});*/
+
