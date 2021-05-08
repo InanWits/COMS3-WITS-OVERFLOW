6a825830621018edfef19b0dd9f0c228242bef6f
SurpriceMasilela <1921674@students.wits.ac.za>
2021-05-08 14:30:53 +0200

    Created tests for the question model

---
+++ tests/models/QuestionModel.test.js
@@ -0,0 +1,56 @@
+const dotenv = require('dotenv');
+const {afterAll, beforeAll, test} = require("@jest/globals");
+
+const database = require("../../utils/services/database");
+
+const QuestionModel = require("../../models/QuestionModel");
+
+const  questionConstants = require('../../utils/constants/QuestionConstants');
+
+const databaseBuilder = require("../../database/WTO_DB_BUILDER");
+
+beforeAll(async (done) => {
+    dotenv.config();
+    database.connectToDatabase().then(
+        () => {
+            databaseBuilder.createDatabase();
+            done();
+        },
+        () => { done(); });
+});
+
+afterAll(async () => {
+    await database.getConnection().end();
+});
+
+test("insertQuestion_validDataAsInput_questionInserted", () => {
+    const questionData = {
+        [questionConstants.student_id] : 1626,
+        [questionConstants.topic_id] : 1,
+        [questionConstants.question] :"some question",
+        //We need confirmation here
+        [questionConstants.question_picture_url] : "wwefghhyjyjugg",
+        //[questionConstants.post_date_time] : "2021-05-08 11:33:04"
+    };
+
+    QuestionModel.insertQuestion(questionData).then(
+        (Question_id) => {
+            expect(Question_id).toBeGreaterThan(0);
+        });
+});
+
+test("insertQuestion_invalidDataAsInput_QuestionInsertFailed", () => {
+    const questionData = {
+        "something" : 1626,
+        [questionConstants.topic_id] : 1,
+        [questionConstants.question] :"some question",
+        [questionConstants.question_picture_url] : "wwefghhyjyjugg",
+        [questionConstants.post_date_time] : "2021-05-08 11:33:04"
+    };
+
+    QuestionModel.insertQuestion(questionData).then(
+        () => {}, //for success
+        (err) => {
+            expect(err).not.toBe(null);
+        });
+});
