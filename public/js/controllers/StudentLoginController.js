288d6d9588bf61e905170dd159f550929792e868
Thabelang02 <1918469@students.wits.ac.za>
2021-05-17 18:22:53 +0200

    implemented user info caching

--- public/js/controllers/StudentLoginController.js
+++ public/js/controllers/StudentLoginController.js
@@ -12,10 +12,14 @@ $(window).on('load', () => {
 
         //we get a promise after the request
         loginStudent(studentData.user_name, studentData).then(
-            (studentId) => { //promise kept
+            (studentObj) => { //promise kept
                 location.href = "html/HomePage.html";
                 //save student Id in local storage
-                localStorage.setItem(KEY_STUDENT_ID, studentId.toString());
+
+                localStorage.setItem(KEY_STUDENT_ID, studentObj.student_id);
+                localStorage.setItem(KEY_STUDENT_F_NAME, studentObj.first_name);
+                localStorage.setItem(KEY_STUDENT_L_NAME, studentObj.last_name);
+
             },
             (err) => { //promise broken
                 alert(err.responseText);
