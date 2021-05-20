99768467c1e0bb117130c288899d9e041b4b1a9f
Koketso31 <1908676@students.wits.ac.za>
2021-05-20 19:31:42 +0200

    created model for vote answer

--- public/js/models/AnswerModel.js
+++ public/js/models/AnswerModel.js
@@ -2,5 +2,9 @@ const answerEndPoint = 'Answers';
 
 const createAnswer = (AnswerData) => {
     return sendPostRequest(answerEndPoint, AnswerData);
+}
 
+const voteAnswer = (voteData) => {
+    const endPoint = `${answerEndPoint}/vote`;
+    return sendPostRequest(endPoint, voteData);
 }
