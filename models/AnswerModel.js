26373c5f47cce5f9b50ff1cc08709a136bb5a099
SurpriceMasilela <1921674@students.wits.ac.za>
2021-06-01 23:03:10 +0200

    Added student constants to answer model

--- models/AnswerModel.js
+++ models/AnswerModel.js
@@ -1,4 +1,5 @@
 const db = require('../utils/services/database');
+const studentConstants = require('../utils/constants/StudentConstants');
 
 const answerConstants = require('../utils/constants/AnswerConstants');
 
@@ -45,8 +46,14 @@ module.exports = {
                 [answerConstants.question_id] : questionId
             };
 
-            const selectAnswers = queryHelper.buildSelectQuery(answerConstants.table_name, [], whereCondition);
+            //const selectAnswers = queryHelper.buildSelectQuery(answerConstants.table_name, [], whereCondition);
+            const ListOfTables = [answerConstants.table_name,studentConstants.table_name];
+            const ListOfOnJoinCols = [answerConstants.student_id, studentConstants.student_id];
 
+            const selectAnswers = queryHelper.buildAssociatedInnerJoin(ListOfTables
+                , ListOfOnJoinCols
+                ,[[answerConstants.post_date_time,answerConstants.answer,answerConstants.answer_picture_url],[studentConstants.user_name]]
+                , true, [], whereCondition);
             console.log(selectAnswers);
 
             db.getConnection().query(selectAnswers, (err, result) => {
